6d64fcb9ce98993d8d331115cd721eceb5fee15d
girvel <widauka@ya.ru>
2024-01-27 23:24:29 +0000

    Rc<str> is strange

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
-fn call_function(f: fn() -> ()) {
-    println!("[DEBUG] Size: {}", std::mem::size_of_val(&f));
-    f();
-}
-
-fn call_function_cheaply<F: FnOnce()>(f: F) {
-    println!("[DEBUG] Size: {}", std::mem::size_of_val(&f));
-    f();
-}
-
-fn say_something() {
-    println!("Something");
-}
+use std::rc::Rc;
 
 fn main() {
-    call_function(|| println!("Hello world!"));
-    call_function_cheaply(|| println!("Hello again!"));
-
-    let mut input_string = String::new();
-    std::io::stdin().read_line(&mut input_string).unwrap();
-
-    call_function_cheaply(if input_string.trim() == "something" { say_something } else { || println!("Nothing.") });
+    let rc_string: Rc<str>;
+    {
+        let string = String::from("Hello, ") + "world";
+        rc_string = Rc::from(string.as_str());
+    }
+    println!("{}", rc_string);
 }
