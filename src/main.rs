e0e0bad0afe3617bc4d14f51e6464a6e2cb68a6d
girvel <widauka@ya.ru>
2024-01-28 00:09:37 +0000

    Figured out Rc<str>
    It was cloning the data.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use std::rc::Rc;
 
+#[derive(Debug, Clone)]
+struct Container(i32);
+
 fn main() {
-    let rc_string: Rc<str>;
+    //let rc_string: Rc<str>;
+    //{
+    //    let string = String::from("Hello, ") + "world";
+    //    rc_string = Rc::from(string.as_str());
+    //}
+    //println!("{}", rc_string);
+    
+    let rc_slice: Rc<[Container]>;
     {
-        let string = String::from("Hello, ") + "world";
-        rc_string = Rc::from(string.as_str());
+        let slice: &[Container] = &[Container(42)];
+        rc_slice = Rc::from(slice);
+
     }
-    println!("{}", rc_string);
+    println!("{:?}", rc_slice);
 }
